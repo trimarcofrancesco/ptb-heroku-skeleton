8360b7c707c3d8a7efd0cc64445cce1c6d0b96c4
trimarcofrancesco <47721676+trimarcofrancesco@users.noreply.github.com>
2019-02-18 00:23:43 +0100

    Update herokubot.py

--- herokubot.py
+++ herokubot.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import apiai, json
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -9,7 +10,18 @@ def start(bot, update):
 
 
 def echo(bot, update):
-    update.effective_message.reply_text(update.effective_message.text)
+    request = apiai.ApiAI('484d9406e9004089b22adbff17df46fc').text_request() # Dialogflow API Token
+    request.lang = 'it' 
+    request.session_id = 'Small-Talk' # ID dialog session (for bot training)
+    request.query = update.message.text # Send request to AI with the user message
+    responseJson = json.loads(request.getresponse().read().decode('utf-8'))
+    response = responseJson['result']['fulfillment']['speech'] # Take JSON answer
+    if response:
+        bot.send_message(chat_id=update.message.chat_id, text=response)
+    else:
+        bot.send_message(chat_id=update.message.chat_id, text='I dont understand!')
+        
+    # update.effective_message.reply_text(update.effective_message.text)
 
 def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"', update, error)
